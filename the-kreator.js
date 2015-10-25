96ab3b5e04be84c8e52140e69a0363f8be97966a
Fedi 2 <Fedi 2>
2015-10-26 00:48:18 +0100

    Version du 26.10 01h

---
+++ the-kreator.js
@@ -0,0 +1,129 @@
+Theodoer = new Mongo.Collection("theodoer");
+
+if (Meteor.isClient) {
+	
+	
+	
+		
+	// Code modifier accounts.ui.config
+
+	
+	Accounts.ui.config({
+		passwordSignupFields: "USERNAME_ONLY"
+		
+	}); 
+
+	
+	
+	// Code pour la liste de Theodoer
+
+	Template.index.helpers({
+
+		theodoers: function () {
+			// Show newest Theodoer at the top
+			return Theodoer.find({}, {sort: {createdAt: -1}});
+		}
+	});
+  
+	Template.createForm.events({
+
+		// handle the form submission 
+		'submit form': function(event) {
+
+			// stop the form from submitting
+			event.preventDefault();
+
+			// get the data we need from the form
+			var firstname = event.target.prenom.value;
+			var name = event.target.nom.value;
+			var email = event.target.email.value;
+			// create the new Theodoer
+			if(firstname != "" && name != "" && email != ""){
+				Meteor.call("createTheodoer", firstname, name, email);
+			}
+		}
+	}); 
+ 
+}   
+
+if (Meteor.isServer) {
+	Meteor.startup(function () {
+		// code to run on server at startup
+		return Meteor.methods({
+
+			removeAllTheodoer: function() {
+
+				return Theodoer.remove({});
+			}
+
+		});
+	});
+	
+	//Prevent new accounts creation
+    Accounts.validateNewUser(function() {
+		return false;
+	});
+	 
+	
+}
+
+
+
+Meteor.methods({
+	createTheodoer: function (firstname, name, email) {
+		// Make sure the user is logged in before creating a theodoer
+		if (! Meteor.userId()) {
+			throw new Meteor.Error("not-authorized");
+		}
+		
+		Theodoer.insert({
+			prenom: firstname,
+			nom: name,
+			email: email,	
+			createdAt: new Date()
+		});
+	},
+});
+
+/*
+
+Router.route('/', function () {
+  this.render('Main');
+});
+
+Router.route('/Formulaire', function () {
+  this.render('Formulaire');
+});
+
+Router.route('/index', function () {
+  this.render('index');
+});
+
+
+*/
+
+
+Router.map(function() {
+  this.route('Main', {path: '/'});
+  this.route('index');
+  this.route('Formulaire');
+});
+
+var mustBeSignedIn = function(pause) {
+  if (!(Meteor.user() || Meteor.loggingIn())) {
+    Router.go('Main');
+  } else {
+    this.next();
+  }
+};
+
+var goToDashboard = function(pause) {
+  if (Meteor.user()) {
+    Router.go('index');
+  } else {
+    this.next();
+  }
+};
+
+Router.onBeforeAction(mustBeSignedIn, {except: ['Main']});
+Router.onBeforeAction(goToDashboard, {only: ['Main']});
