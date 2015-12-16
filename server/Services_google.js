e1f4b2220579c9c29e6c409aae29f32bc14fc45a
Fedi 2 <Fedi 2>
2015-12-16 14:02:47 +0100

    creation du service

---
+++ server/Services_google.js
@@ -0,0 +1,7 @@
+ ServiceConfiguration.configurations.upsert({service: 'google'}, {
+    $set: {
+      clientId: '337810751573-mr60nns3au4vaf24i8ashkdcdfr8i0uo.apps.googleusercontent.com',
+      secret: 'wmvRWEszSXToFQYA5Nc-DAWU',
+      loginStyle: 'popup'
+    }
+  });
