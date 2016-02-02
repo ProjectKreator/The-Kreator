b94e37b46ad3e710c3e9b3c1af107a4a94feed2d
pvtalbot <paul.valentin.talbot@gmail.com>
2016-02-02 13:22:08 +0100

    google en dynamique

--- server/Services_google.js
+++ server/Services_google.js
@@ -1,7 +1,7 @@
- ServiceConfiguration.configurations.upsert({service: 'google'}, {
+ServiceConfiguration.configurations.upsert({service: 'google'}, {
     $set: {
-      clientId: '337810751573-mr60nns3au4vaf24i8ashkdcdfr8i0uo.apps.googleusercontent.com',
-      secret: 'wmvRWEszSXToFQYA5Nc-DAWU',
+      clientId: Meteor.settings.google.clientId,
+      secret: Meteor.settings.google.secret,
       loginStyle: 'popup'
     }
   });
