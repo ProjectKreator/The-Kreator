b45e06469d0ad8f191049c83155bdc3d0fde571b
ChugonJD <jerem.dardour@gmail.com>
2016-02-03 20:08:32 +0100

    dernieres modifs

--- methods/API's/Google/GoogleRequest.js
+++ methods/API's/Google/GoogleRequest.js
@@ -3,6 +3,8 @@ Meteor.methods({
         var res = "";
         var check = false;
         var i = 0;
+        var apiKey = Meteor.settings.google.clientId;
+        var authorize = "Bearer " + Meteor.user().services.google.accessToken;
         
         var addLetter = function() {
             return (res + nom.charAt(i).toLowerCase())
@@ -12,6 +14,9 @@ Meteor.methods({
             // requete http ou methode google pour voir si l'adresse existe deja 
             var url = "https://www.googleapis.com/admin/directory/v1/users/" + res + "@kretaor.in";
             HTTP.get(url, {
+                "data": {
+                    "Authorization": authorize
+                },    
             }, function(error, response){
                 if(error){
                     console.log(error);
@@ -22,12 +27,17 @@ Meteor.methods({
             );
         };
         
-        while (!check) {
+        while (!check && i < 1) {
             res = addLetter()
             checkEmailApi()
             i += 1;
         }
           
         console.log(res)
+    },
+    
+    createEmail: function (prenom, nom) {
+      
     }
+   
 });
--- the-kreator.js
+++ the-kreator.js
@@ -1,8 +1,10 @@
 
 if (Meteor.isClient) {
-	
-	
- }   
+// scope pour autoriser l'application à gerer les users Google apps
+    var scopes = ['https://www.googleapis.com/auth/admin.directory.user']
+    Accounts.ui.config({'requestPermissions':{'google':scopes}});
+}
+
 
 if (Meteor.isServer) {
 	
