86f470f4638a2b1ba10837008b64d480ef35c302
pvtalbot <paul.valentin.talbot@gmail.com>
2016-02-05 09:03:11 +0100

    reorganisation (methodes avec pages associees)

---
+++ Methodes/Services_google.js
@@ -0,0 +1,8 @@
+ServiceConfiguration.configurations.remove({service : 'google'});
+ServiceConfiguration.configurations.upsert({service : 'google'}, {
+    $set: {
+	    clientId: Meteor.settings.google.clientId,
+	    secret: Meteor.settings.google.secret,
+	    loginStyle: 'popup'
+	}
+});  
