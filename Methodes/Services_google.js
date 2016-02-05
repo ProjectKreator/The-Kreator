4cbe8385b050e305593b80f7aa3941e336289169
pvtalbot <paul.valentin.talbot@gmail.com>
2016-02-05 10:57:02 +0100

    Revert "reorganisation (methodes avec pages associees)"
    This reverts commit 86f470f4638a2b1ba10837008b64d480ef35c302.

--- Methodes/Services_google.js
+++
@@ -1,8 +0,0 @@
-ServiceConfiguration.configurations.remove({service : 'google'});
-ServiceConfiguration.configurations.upsert({service : 'google'}, {
-    $set: {
-	    clientId: Meteor.settings.google.clientId,
-	    secret: Meteor.settings.google.secret,
-	    loginStyle: 'popup'
-	}
-});  
