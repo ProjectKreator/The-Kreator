f774c51e52aa9ddc0077702f6d4c6ac2d50c9cf5
ChugonJD <jerem.dardour@gmail.com>
2016-02-24 18:54:51 +0100

    Test.js ouvre une fenetre d'auth

--- client/Templates/Test.js
+++ client/Templates/Test.js
@@ -1,6 +1,15 @@
 Template.Test.events({
 	'click [name=testGoogle]' : function(event){
-        console.log(Meteor.user());
-	}
-
+           
+        event.preventDefault();
+        
+        var CLIENT_ID = Meteor.settings.public.google.clientId;
+        var REDIRECT_URL = Meteor.settings.public.google.redirectUrl;
+        var acces_type = "offline"; // will return a refresh token
+        var scope = 'https://www.googleapis.com/auth/admin.directory.user';
+        var adresse = "https://accounts.google.com/o/oauth2/auth?access_type="+ acces_type + "&scope=" + scope + "&response_type=code&client_id=" + CLIENT_ID +"&redirect_uri=" +REDIRECT_URL;
+        console.log(adresse);
+        window.open(adresse,'_blank','newwindow', 'width=200', 'height=100');
+        
+    },
 });
--- methods/API's/Google/GoogleRequest.js
+++ methods/API's/Google/GoogleRequest.js
@@ -1,4 +1,61 @@
 Meteor.methods({
+    
+    testToken : function(){
+       // var google = Meteor.npmRequire('C:/Users/Jeremy/The-Kreator/.meteor/local/isopacks/npm-container/npm/node_modules/googleapis/lib/googleapis.js');
+        var google = Meteor.npmRequire('../node_modules/googleapis/lib/googleapis.js');
+        var OAuth2Client = google.auth.OAuth2;
+        var admin = google.admin('directory_v1');
+
+        // Client ID and client secret are available at
+        // https://code.google.com/apis/console
+        var CLIENT_ID = Meteor.settings.google.clientId;
+        var CLIENT_SECRET = Meteor.settings.google.secret;
+        var REDIRECT_URL = 'http://localhost:80';
+
+        var oauth2Client = new OAuth2Client(CLIENT_ID, CLIENT_SECRET, REDIRECT_URL);
+
+
+        function getAccessToken(oauth2Client, callback) {
+          // generate consent page url
+          var url = oauth2Client.generateAuthUrl({
+            access_type: 'offline', // will return a refresh token
+            scope: 'https://www.googleapis.com/auth/admin.directory.user' // can be a space-delimited string or an array of scopes
+          });
+            
+            // request access token
+            oauth2Client.getToken(code, function(err, tokens) {
+              console.log("err before "+err);
+              // set tokens to the client
+              // TODO: tokens should be set by OAuth2 client.
+              oauth2Client.setCredentials(tokens);
+              callback();
+            }); 
+        }
+
+        // retrieve an access token
+
+        getAccessToken(oauth2Client, function(e, r){
+            if(e){
+                console.log("error "+e);
+            }
+            else{
+                console.log("response "+r);
+            }
+
+        });
+        
+        /*getAccessToken(oauth2Client, function() {
+          // retrieve user profile
+          plus.people.get({ userId: 'me', auth: oauth2Client }, function(err, profile) {
+            if (err) {
+              console.log('An error occured', err);
+              return;
+            }
+            console.log(profile.displayName, ':', profile.tagline);
+          });
+        });*/
+    },
+    
     checkEmail: function (prenom,nom) {
         var res = prenom;
         var stopCheckEmail = false;
@@ -43,4 +100,6 @@ Meteor.methods({
       
     }
    
+    
+    
 });
