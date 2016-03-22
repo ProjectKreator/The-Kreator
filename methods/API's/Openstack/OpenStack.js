220b03058aa5349ab04552b097fb92783cb489a3
Ubuntu <ubuntu@kreator.novalocal>
2016-03-22 10:19:29 +0000

    Fix Openstack request

--- methods/API's/Openstack/OpenStack.js
+++ methods/API's/Openstack/OpenStack.js
@@ -1,23 +1,24 @@
 Meteor.methods({
-	inviteToOpenstack : function(nom, prenom, email){
+	inviteToOpenstack : function(username, email){
         //var utilisateur?? = prenom + " " + nom;
         var apiKeyOpenStack = Meteor.settings.public.OpenStack.apiKey ;
         var projectIDOpenStack = Meteor.settings.public.OpenStack.projectID
         //var adresse = "https://api.synalabs.net/api/v1/openstack/identity/monProjectID/users/";
         //var adresse = "https://api.synalabs.net/api/v1/openstack/identity/" + projectIDOpenStack + "/users/";
-		HTTP.post(adresse,{
-			header:{
-				'Content-Type': 'application/x-www-form-urlencoded',
-                Accept: 'application/json',
-                Authorization: 'Synalabs-API' + apiKeyOpenStack
+	HTTP.post(adresse,{
+	    header:{
+		'Content-Type': 'application/x-www-form-urlencoded',
+                'Accept': 'application/json',
+                'Authorization': 'Synalabs-API ' + apiKeyOpenStack
 			},
             data:{
-                username: "utilisateur&email=toto%40toto.com" //tbc?
-			}},
-			function(e,r){
-				if(e){
-					console.log(e);
-				} else {
+                username: username,
+                email: email
+	    }
+        }, function(e,r){
+	    if(e){
+		console.log(e);
+            } else {
 					console.log(r);
 					Theodoer.update({current:true},
 						{$set:{"requestOpenStack.status" : r.statusCode}}
