3e6dd3b8d2832393f97ce328f44a2a60f63c266f
pvtalbot <paul.valentin.talbot@gmail.com>
2016-03-23 15:59:27 +0100

    feature afficher uniquement ses gars

--- client/Templates/Index/index.js
+++ client/Templates/Index/index.js
@@ -5,9 +5,51 @@
 		theodoers: function () {
 			// Show newest Theodoer at the top
 			var currentUser = Meteor.userId();
-			return Theodoer.find({}, {sort: {createdAt: -1}});
+			var printAllUsers;
 
-//			return Theodoer.find({createdBy : currentUser}, {sort: {createdAt: -1}});
-		}
+			try{
+	    		printAllUsers = !Meteor.user().profile.noOtherUsersPrinting;
+	    	} catch(e){
+	  		  	printAllUsers = false;
+	   		}
+
+	   		if(printAllUsers){
+				return Theodoer.find({}, {sort: {createdAt: -1}});
+			} else {
+				return Theodoer.find({createdBy : currentUser}, {sort: {createdAt: -1}});
+			}
+		},
+
+		'checked': function(){
+        var isCompleted;
+	    try{
+	    	isCompleted = Meteor.user().profile.noOtherUsersPrinting;
+	    } catch(e){
+	    	isCompleted = false;
+	    }
+
+        if(isCompleted){
+            return "checked";
+        } else {
+            return "";
+        }
+    }
 	});
+
+Template.index.events({
+	'change [type=checkbox]': function(){
+	    var dontPrintOtherUsers;
+	    try{
+	    	dontPrintOtherUsers = Meteor.user().profile.noOtherUsersPrinting;
+	    } catch(e){
+	    	dontPrintOtherUsers = false;
+	    }
+
+	    if(dontPrintOtherUsers){
+	        Meteor.users.update({ _id: Meteor.userId() }, {$set: { "profile.noOtherUsersPrinting" : false }});
+	    } else {
+	        Meteor.users.update({ _id: Meteor.userId() }, {$set: { "profile.noOtherUsersPrinting" : true }});
+	    }
+	}
+});
   
